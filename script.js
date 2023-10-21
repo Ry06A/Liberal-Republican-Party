f4de6b75e2ac946a67fbbd3737f15265efed83e1
Ryo <140031229+Ry06A@users.noreply.github.com>
2023-10-21 22:45:56 +0900

    script.js の更新

--- script.js
+++
@@ -1,5 +0,0 @@
-const menuButton = document.querySelector('.hamburger-menu');
-const nav = document.querySelector('.nav');
-menuButton.addEventListener('click', () => {
-  nav.classList.toggle('active');
-});
