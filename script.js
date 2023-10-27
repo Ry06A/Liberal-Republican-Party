6485afc0d47ef2c8d652fec80b6e3ab447cc5b91
Ryo <140031229+Ry06A@users.noreply.github.com>
2023-10-27 13:24:25 +0900

    update

---
+++ script.js
@@ -0,0 +1,14 @@
+$(function() {
+
+    $('.hamburger').click(function() {
+  
+      $(this).toggleClass('active');
+  
+      if ($(this).hasClass('active')) {
+        $('#navi').addClass('active');
+      } else {
+        $('#navi').removeClass('active');
+      }
+  
+    });
+  });
