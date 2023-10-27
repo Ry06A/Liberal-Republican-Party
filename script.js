fdac3dd98d30f5a567791db70b78bae73c45bfc7
Ryo <140031229+Ry06A@users.noreply.github.com>
2023-10-27 13:25:58 +0900

    update

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
 $(function() {
-
-    $('.hamburger').click(function() {
-  
-      $(this).toggleClass('active');
-  
-      if ($(this).hasClass('active')) {
-        $('#navi').addClass('active');
-      } else {
-        $('#navi').removeClass('active');
-      }
-  
-    });
+  $('.hamburger').click(function() {
+    $(this).toggleClass('active');
+    if ($(this).hasClass('active')) {
+    $('#navi').addClass('active');
+    } 
+    else {
+      $('#navi').removeClass('active');
+    }
   });
+});
