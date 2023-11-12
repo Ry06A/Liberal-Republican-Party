4e3e735c4a4080fa7e175203278675452ed55156
Ryo <140031229+Ry06A@users.noreply.github.com>
2023-11-12 14:04:24 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-$(function () {
-  $('.hamburger').click(function () {
-    $(this).toggleClass('active');
-    if ($(this).hasClass('active')) {
-      $('#navi').addClass('active');
-    } 
-    else {
-      $('#navi').removeClass('active');
-    }
-  });
-});
+
